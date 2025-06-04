68f32dfa9afc2e62f579bf2b3189b3a48eedc597
Lorenzo Sciandra <notkelset@kelset.dev>
2025-06-04 12:30:59 +0100

    wasn't heavy enough

--- src/heavyTask.ts
+++ src/heavyTask.ts
@@ -2,7 +2,7 @@ export function setupHeavyTask(element: HTMLButtonElement) {
   element.innerHTML = 'Compute sum';
   element.addEventListener('click', () => {
     let sum = 0;
-    for (let i = 1; i <= 100000; i++) {
+    for (let i = 1; i <= 1000000000; i++) {
       sum += i;
     }
     element.innerHTML = `sum is ${sum}`;
