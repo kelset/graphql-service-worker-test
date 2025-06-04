8ef4f4dc5a90c6af9250fd9023c99fc0528319d2
Lorenzo Sciandra <notkelset@kelset.dev>
2025-06-04 16:59:30 +0100

    Add logging for GraphQL service worker (#10)

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'graphql-cache-v1';
+const GRAPHQL_ENDPOINT = 'https://beta.pokeapi.co/graphql/v1beta';
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  if (request.method === 'POST' && request.url === GRAPHQL_ENDPOINT) {
+    console.log('[Service Worker] Intercepting GraphQL request');
+    event.respondWith(handleGraphQLRequest(request));
+  }
+});
+
+async function handleGraphQLRequest(request) {
+  const reqClone = request.clone();
+  const body = await reqClone.text();
+  const cacheKey = new Request(`${request.url}?body=${body}`, { method: 'GET' });
+  const cache = await caches.open(CACHE_NAME);
+  const cached = await cache.match(cacheKey);
+  if (cached) {
+    console.log('[Service Worker] Serving response from cache');
+    return cached;
+  }
+  console.log('[Service Worker] Fetching from network');
+  const response = await fetch(request);
+  cache.put(cacheKey, response.clone());
+  console.log('[Service Worker] Cached response');
+  return response;
+}
