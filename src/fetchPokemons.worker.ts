effef9b2759cdba6739a138c500843f29e3b4d0d
Lorenzo Sciandra <lorenzo.sciandra@gmail.com>
2025-06-09 12:28:37 +0100

    Add console logs for worker interactions

---
+++ src/fetchPokemons.worker.ts
@@ -0,0 +1,79 @@
+/// <reference lib="webworker" />
+
+import { gql, request } from 'graffle'
+
+interface Species {
+  name: string
+  id: number
+}
+
+interface Generation {
+  name: string
+  pokemon_species: { aggregate: { count: number } }
+}
+
+interface QueryResult {
+  gen3_species: Species[]
+  generations: Generation[]
+  [key: string]: unknown
+}
+
+const endpoint = 'https://beta.pokeapi.co/graphql/v1beta'
+
+const query = gql`
+  query bigPokeAPIquery {
+    gen3_species: pokemon_v2_pokemonspecies(
+      where: { pokemon_v2_generation: { name: { _eq: "generation-iii" } } }
+      order_by: { id: asc }
+    ) {
+      name
+      id
+      pokemon: pokemon_v2_pokemons(limit: 1) {
+        height
+        weight
+        stats: pokemon_v2_pokemonstats {
+          base_stat
+          pokemon_v2_stat {
+            name
+          }
+        }
+        abilities: pokemon_v2_pokemonabilities(limit: 2) {
+          pokemon_v2_ability {
+            name
+          }
+        }
+        moves: pokemon_v2_pokemonmoves(limit: 5) {
+          pokemon_v2_move {
+            name
+          }
+        }
+      }
+    }
+    generations: pokemon_v2_generation {
+      name
+      pokemon_species: pokemon_v2_pokemonspecies_aggregate {
+        aggregate {
+          count
+        }
+      }
+    }
+  }
+`
+
+self.onmessage = async () => {
+  console.log('Worker: received fetch command')
+  console.log('Worker: fetching data...')
+  try {
+    const data = await request<QueryResult>({
+      url: endpoint,
+      document: query,
+    })
+    console.log('Worker: posting fetched data')
+    self.postMessage({ data })
+  } catch (err) {
+    console.error('Worker: error fetching data', err)
+    self.postMessage({ error: (err as Error).message })
+  }
+}
+
+export default null as any
